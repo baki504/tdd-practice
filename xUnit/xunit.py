e5a630516df120e5680249ae319958b4ed52b0ff
baki504 <baki504@msn.com>
2025-08-19 07:57:07 +0900

    159. before refactor

--- xUnit/xunit.py
+++ xUnit/xunit.py
@@ -2,7 +2,11 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
+    def setUp(self):
+        pass
+
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
@@ -11,6 +15,9 @@ class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
         super().__init__(name)
+
+    def setUp(self):
+        self.wasSetUp = 1
 
     def testMethod(self):
         self.wasRun = 1
@@ -23,5 +30,11 @@ class TestCaseTest(TestCase):
         test.run()
         assert test.wasRun
 
+    def testSetUp(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert test.wasSetUp
+
 
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
